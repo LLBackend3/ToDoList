08d6d8c16dd8f0c15a739ded619e7eb0dfaf9359
gibeom00 <pkb3142@naver.com>
2025-04-30 00:08:02 +0900

    commit

---
+++ src/test/java/com/springboot/teamproject/TeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.teamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
