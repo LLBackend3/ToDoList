0290ec93b5a40dd4282c5f4c1c4f2f37075564dc
wlsdydlee <eagie0529@naver.com>
2025-04-30 16:09:26 +0900

    dto

--- src/main/java/com/springboot/teamproject/dto/ToDoRequestDto.java
+++ src/main/java/com/springboot/teamproject/dto/ToDoRequestDto.java
@@ -1,4 +1,53 @@
 package com.springboot.teamproject.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class ToDoRequestDto {
+    private String userName;
+    private String title;
+    private String description;
+    private boolean completed;
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean isCompleted() {
+        return this.completed;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDoRequestDto(userName=" + this.getUserName() +
+                ", title=" + this.getTitle() +
+                ", description=" + this.getDescription() +
+                ", completed=" + this.isCompleted() + ")";
+    }
 }
--- src/main/java/com/springboot/teamproject/dto/ToDoResponseDto.java
+++ src/main/java/com/springboot/teamproject/dto/ToDoResponseDto.java
@@ -1,4 +1,63 @@
 package com.springboot.teamproject.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class ToDoResponseDto {
+    private Long id;
+    private String userName;
+    private String title;
+    private String description;
+    private boolean completed;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean isCompleted() {
+        return this.completed;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDoResponseDto(id=" + this.getId() +
+                ", userName=" + this.getUserName() +
+                ", title=" + this.getTitle() +
+                ", description=" + this.getDescription() +
+                ", completed=" + this.isCompleted() + ")";
+    }
 }
